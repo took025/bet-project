965be9a310b2ff3e6892af53350d3758911e3954
took025 <tokofxaladze@gmail.com>
2019-09-16 02:35:09 -0700

    sd

--- js/script.js
+++
@@ -1,7 +0,0 @@
-var burgerIcon = document.getElementById("burger");
-var burgerMenu = document.getElementById("burger-menu");
-
-burgerIcon.addEventListener("click",  function(){
-	burgerIcon.classList.toggle("active")
-	burgerMenu.classList.toggle("active")
-});
